a048028793277f7677666c46186f89e49cbc0d3d
aleksmn <al-mn@yandex.ru>
2021-06-30 12:24:18 +0300

    initial

--- py-rss-to-database.py
+++ py-rss-to-database.py
@@ -1,6 +1,6 @@
 from xml.etree import ElementTree as etree
-from collections import Counter
-from bs4 import BeautifulSoup
+# from collections import Counter
+# from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 import requests
@@ -9,34 +9,10 @@ import sys
 def time_to_string(time_object):
     return datetime.strftime(time_object, '%H:%M') # '%d.%m.%y %H:%M' --> 26.05.20 13:27
 
-
 def sort_by_time(articles, reverse=False):
     """Sort articles by time, by default do not reverse: 
     most recent at the bottom."""
     return sorted(articles, key=lambda k: k['time'], reverse=reverse)
-
-
-
-URLS = [
-    'https://www.interfax.ru/rss.asp',
-    'http://tass.ru/rss/v2.xml'
-]
-
-
-if len(sys.argv) > 1:
-    num = sys.argv[1]
-else:
-    #num = input(f'Введите количество источников (не более {len(URLS)}): ')
-    num = len(URLS)
-
-try: 
-    num = int(num)
-except:
-    num = len(URLS)
-
-articles = []
-
-print(f'\nПодключаемся к источникам новостей ({num} из {len(URLS)})...')
 
 def get_items_from_url(url):
     """Getting items from URL of RSS feed"""
@@ -54,34 +30,68 @@ def get_article(entry):
     article['url'] = entry.findtext('guid')
     return article
 
-try:
-    for url in URLS[:num]:
-        items = get_items_from_url(url)
+def clear_from_spam(articles):
+    """Clear articles from spam based on url"""
+    spam_list = ['novosti-partnerov']
+    for w in spam_list:
+        articles = [x for x in articles if w not in x['url']]
+    return articles
 
-        for entry in items:
-            articles.append(get_article(entry))
-
-        
-    articles = sort_by_time(articles)
-
-    # Выводим время публикации и заголовки на экран
+def print_titles(articles):
+    string_limit = 86 #set limit of characters in line
+    sleep_time = 0.01
     for article in articles:
-        if 'novosti-partnerov' in article['url']:
-            continue
         time_output = time_to_string(article['time'])
         title_output = article['title']
         url_output = article['url']
         
-        string_limit = 86
         if len(title_output) > string_limit:
                 title_output = title_output[:string_limit-1] + '~'
-                      
-        # print(f"[{time_output}] {title_output}\n{' ' * 8}{url_output}")
+                        
+        print(f"[{time_output}] {title_output}\n{' ' * 8}{url_output}")
 
         # Compact view:
-        print(f"[{time_output}] {title_output}")
+        # print(f"[{time_output}] {title_output}")
+        time.sleep(sleep_time)    
 
-        time.sleep(0.01)
+def get_num_feeds():
+    if len(sys.argv) > 1:
+        num = sys.argv[1]
+    else:
+        # num = input(f'Введите количество источников (не более {len(URLS)}): ')
+        num = len(URLS)
+
+    try: 
+        num = int(num)
+        if num > len(URLS):
+            num = len(URLS)
+            print("Максимальное количестов источников: ", num)
+    except:
+        num = len(URLS)
+
+    return num
+
+URLS = [
+    'https://www.interfax.ru/rss.asp',
+    'http://tass.ru/rss/v2.xml'
+]
+
+num_feeds = get_num_feeds()
+
+print(f'Подключаемся к источникам новостей ({num_feeds} из {len(URLS)})...')
+
+articles = []
+
+try:
+    for url in URLS[:num_feeds]:
+        for entry in get_items_from_url(url):
+            articles.append(get_article(entry))
+
+        
+    articles = sort_by_time(clear_from_spam(articles))
+
+    # Выводим время публикации и заголовки на экран
+    print_titles(articles)
 
 except requests.exceptions.ConnectionError as error:
     print('Нет соединения с интернетом.')
