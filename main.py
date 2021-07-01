2bd67fde234a4463f2140090457e03212d60c51d
aleksmn <al-mn@yandex.ru>
2021-07-01 13:27:21 +0300

    add feednames to db

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from myrsslib import *
 
-URLS = [
-    'https://www.interfax.ru/rss.asp',
-    'http://tass.ru/rss/v2.xml'
+FEEDS = [
+    {'feedname': 'Interfax', 'url': 'https://www.interfax.ru/rss.asp'},
+    {'feedname': 'TASS', 'url': 'http://tass.ru/rss/v2.xml'},
 ]
 
-num_feeds = get_num_feeds(URLS)
-print(f'Подключаемся к источникам новостей ({num_feeds} из {len(URLS)})...')
+num_feeds = get_num_feeds(FEEDS)
+print(f'Подключаемся к источникам новостей ({num_feeds} из {len(FEEDS)})...')
 articles = []
 try:
-    for url in URLS[:num_feeds]:
-        for entry in get_items_from_url(url):
-            articles.append(get_article(entry))
+    for feed in FEEDS[:num_feeds]:
+        for entry in get_items_from_feed(feed):
+            articles.append(get_article(entry, feed))
 
         
     articles = sort_by_time(clear_from_spam(articles))
 
     # Выводим время публикации и заголовки на экран
-    print_titles(articles)
+    # print_titles(articles)
+
     # Добавляем заголовки в базу данных
     add_to_db(articles)
 
--- myrsslib.py
+++ myrsslib.py
@@ -7,12 +7,6 @@ import requests
 import sys
 import sqlite3
 
-# URLS = []
-# URLS = [
-#     'https://www.interfax.ru/rss.asp',
-#     'http://tass.ru/rss/v2.xml'
-# ]
-
 def get_num_feeds(URLS):
     """Getting number of urls to scrap, max value is len of URLs list."""
     if len(sys.argv) > 1:
@@ -37,20 +31,21 @@ def sort_by_time(articles, reverse=False):
     most recent at the bottom."""
     return sorted(articles, key=lambda k: k['time'], reverse=reverse)
 
-def get_items_from_url(url):
+def get_items_from_feed(feed):
     """Getting items from URL of RSS feed"""
-    data = requests.get(url = url).text
-    print (f"Собираем данные с <{url}>...")
+    data = requests.get(url = feed['url']).text
+    print (f"Собираем данные с {feed['feedname']}...")
     RSS = etree.fromstring(data)
     return RSS.findall('channel/item')
 
-def get_article(entry):
+def get_article(entry, feed):
     """Get article from the entry on RSS feed. Return article as dictionary with atributes time, date, url."""
     article = {}
     article['time'] = datetime.strptime(entry.findtext('pubDate'), '%a, %d %b %Y %H:%M:%S %z')
             # Example of 'pubdate':  Tue, 26 May 2020 13:27:00 +0300 
     article['title'] = entry.findtext('title')
     article['url'] = entry.findtext('guid')
+    article['feedname'] = feed['feedname']
     return article
 
 def clear_from_spam(articles):
@@ -77,19 +72,22 @@ def print_titles(articles):
         # print(f"[{time_output}] {title_output}")
         time.sleep(sleep_time)    
 
-
 def add_to_db(articles):
+    """Add article to the news.db"""
     conn = sqlite3.connect('news.db')
     c = conn.cursor()
     
-    # find most recent title in db 
+    # find most recent title in db
+    most_recent_time = ''
     c.execute("SELECT * FROM titles ORDER BY time DESC LIMIT 1")
-    most_recent_time = c.fetchone()[0]
+    most_recent_row = c.fetchone()
+    if most_recent_row is not None:
+        most_recent_time = most_recent_row[0]
 
     for a in articles:
         # add only new titles to db
         if str(a['time']) > most_recent_time:
-            c.execute(f"INSERT INTO titles VALUES ('{a['time']}', '{a['title']}', '{a['url']}')")
+            c.execute(f"INSERT INTO titles VALUES ('{a['time']}', '{a['title']}', '{a['url']}', '{a['feedname']}')")
         else:
             continue
 
@@ -97,22 +95,24 @@ def add_to_db(articles):
     conn.close()
     
 
-
-
-
 if __name__ ==  "__main__":
     # Create db
     conn = sqlite3.connect('news.db')
     c = conn.cursor()
     # c.execute("""CREATE TABLE titles (
+
     #             time text,
     #             title text,
-    #             url text
+    #             url text,
+    #             feedname text
     #         )""")   
     
-    c.execute("SELECT * FROM titles ORDER BY title")
-    for item in c.fetchall():
-        print(item[1])
+    c.execute("SELECT * FROM titles ORDER BY time")
+    for i in c.fetchall():
+        print(i[2], i[3])
 
-    # conn.commit()
-    # conn.close()
+    # c.execute("SELECT * FROM titles ORDER BY time DESC LIMIT 1") 
+    # print(c.fetchone())
+
+    conn.commit()
+    conn.close()
