ec272bf72e506649a8c71470c2ee64fad3a8664d
aleksmn <al-mn@yandex.ru>
2021-07-01 12:27:41 +0300

    add database

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from myrsslib import *
 
+URLS = [
+    'https://www.interfax.ru/rss.asp',
+    'http://tass.ru/rss/v2.xml'
+]
 
-num_feeds = get_num_feeds()
-
+num_feeds = get_num_feeds(URLS)
 print(f'Подключаемся к источникам новостей ({num_feeds} из {len(URLS)})...')
-
 articles = []
-
 try:
     for url in URLS[:num_feeds]:
         for entry in get_items_from_url(url):
@@ -17,6 +18,8 @@ try:
 
     # Выводим время публикации и заголовки на экран
     print_titles(articles)
+    # Добавляем заголовки в базу данных
+    add_to_db(articles)
 
 except requests.exceptions.ConnectionError as error:
     print('Нет соединения с интернетом.')
--- myrsslib.py
+++ myrsslib.py
@@ -5,13 +5,29 @@ from datetime import datetime
 import time
 import requests
 import sys
+import sqlite3
 
+# URLS = []
+# URLS = [
+#     'https://www.interfax.ru/rss.asp',
+#     'http://tass.ru/rss/v2.xml'
+# ]
 
-URLS = [
-    'https://www.interfax.ru/rss.asp',
-    'http://tass.ru/rss/v2.xml'
-]
-
+def get_num_feeds(URLS):
+    """Getting number of urls to scrap, max value is len of URLs list."""
+    if len(sys.argv) > 1:
+        num = sys.argv[1]
+    else:
+        # num = input(f'Введите количество источников (не более {len(URLS)}): ')
+        num = len(URLS)
+    try: 
+        num = int(num)
+        if num > len(URLS):
+            num = len(URLS)
+            print("Максимальное количестов источников: ", num)
+    except:
+        num = len(URLS)
+    return num
 
 def time_to_string(time_object):
     return datetime.strftime(time_object, '%H:%M') # '%d.%m.%y %H:%M' --> 26.05.20 13:27
@@ -61,19 +77,42 @@ def print_titles(articles):
         # print(f"[{time_output}] {title_output}")
         time.sleep(sleep_time)    
 
-def get_num_feeds():
-    if len(sys.argv) > 1:
-        num = sys.argv[1]
-    else:
-        # num = input(f'Введите количество источников (не более {len(URLS)}): ')
-        num = len(URLS)
 
-    try: 
-        num = int(num)
-        if num > len(URLS):
-            num = len(URLS)
-            print("Максимальное количестов источников: ", num)
-    except:
-        num = len(URLS)
+def add_to_db(articles):
+    conn = sqlite3.connect('news.db')
+    c = conn.cursor()
+    
+    # find most recent title in db 
+    c.execute("SELECT * FROM titles ORDER BY time DESC LIMIT 1")
+    most_recent_time = c.fetchone()[0]
 
-    return num
+    for a in articles:
+        # add only new titles to db
+        if str(a['time']) > most_recent_time:
+            c.execute(f"INSERT INTO titles VALUES ('{a['time']}', '{a['title']}', '{a['url']}')")
+        else:
+            continue
+
+    conn.commit()
+    conn.close()
+    
+
+
+
+
+if __name__ ==  "__main__":
+    # Create db
+    conn = sqlite3.connect('news.db')
+    c = conn.cursor()
+    # c.execute("""CREATE TABLE titles (
+    #             time text,
+    #             title text,
+    #             url text
+    #         )""")   
+    
+    c.execute("SELECT * FROM titles ORDER BY title")
+    for item in c.fetchall():
+        print(item[1])
+
+    # conn.commit()
+    # conn.close()
