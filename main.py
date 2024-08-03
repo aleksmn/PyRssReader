811e9f24cc64fef92b0c0dc691fd821b0ffbe529
aleksmn <al-mn@yandex.ru>
2024-08-03 12:21:02 +0300

    add while loop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from myrsslib import *
 
 FEEDS = [
@@ -5,22 +7,34 @@ FEEDS = [
     # {'feedname': 'TASS', 'url': 'http://tass.ru/rss/v2.xml'}
 ]
 
-num_feeds = get_num_feeds(FEEDS)
-print(f'Подключаемся к источникам новостей ({num_feeds} из {len(FEEDS)})...')
-articles = []
-try:
-    for feed in FEEDS[:num_feeds]:
-        for entry in get_items_from_feed(feed):
-            articles.append(get_article(entry, feed))
+def main():
 
-        
-    articles = sort_by_time(clear_from_spam(articles))
+    num_feeds = get_num_feeds(FEEDS)
+    print(f'Подключаемся к источникам новостей ({num_feeds} из {len(FEEDS)})...')
+    articles = []
+    try:
+        for feed in FEEDS[:num_feeds]:
+            for entry in get_items_from_feed(feed):
+                articles.append(get_article(entry, feed))
 
-    # Выводим время публикации и заголовки на экран
-    print_titles(articles)
+            
+        articles = sort_by_time(clear_from_spam(articles))
+        os.system("cls||clear")    
+        # Выводим время публикации и заголовки на экран
+        print_titles(articles)
 
-    # Добавляем заголовки в базу данных
-    # add_to_db(articles)
+        # Добавляем заголовки в базу данных
+        # add_to_db(articles)
 
-except requests.exceptions.ConnectionError as error:
-    print('Нет соединения с интернетом.')
+
+    except requests.exceptions.ConnectionError as error:
+        print('Нет соединения с интернетом.')
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        answer = input("Repeat? (Y/n) ")
+        answer = answer.lower().strip()
+        if answer == "n" or answer == "н":
+            break 
